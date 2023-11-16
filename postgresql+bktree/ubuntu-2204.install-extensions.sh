422704fe7a3fe642bec4b50bc80e9ea88e828947
Claudius Korzen <cldskrzn@gmail.com>
2023-11-16 15:31:27 +0000

    Install bktree extension in template1 database.

--- postgresql+bktree/ubuntu-2204.install-extensions.sh
+++ postgresql+bktree/ubuntu-2204.install-extensions.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOF
+# CREATE DATABASE actually works by copying the existing database "template1".
+# Install bktree in this database, so this extension is available from all databases.
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "template1" <<EOF
 create extension bktree;
 select * FROM pg_extension;
 EOF
